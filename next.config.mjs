b0cd58fa2db6ff400d9c14cec162b1d4f5919639
IlincaMurarescu <ilincamurarescu@gmail.com>
2026-05-28 00:13:34 +0300

    fix issues

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: "/portfolio",
+  assetPrefix: "/portfolio/",
+  trailingSlash: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
